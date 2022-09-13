095b5b5cfe6df6b610e4578d6b6e755590c63ec2
Akanksha Sharma <68657899+akankshaSha@users.noreply.github.com>
2022-09-13 20:32:57 +0530

    query 1

---
+++ Simple DQL/Query.sql
@@ -0,0 +1,8 @@
+/*
+Simple Queries (DQL: SELECT FROM WHERE):
+*/
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM salaries WHERE emp_no=10001;
+SELECT title FROM titles WHERE emp_no=10006;
+
